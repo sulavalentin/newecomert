6b58259750f67b739ba2f2939447b5786c999b8f
valintin <valintin@192.168.56.1>
2017-04-20 15:33:18 +0300

    compare repair bug

--- app/Compare.php
+++ app/Compare.php
@@ -8,31 +8,40 @@ use DB;
 class Compare extends Model
 {
     public function getCompare(){
-        return DB::table('products')
-                ->select('products.*', 'images.address')
-                ->leftJoin("images",function($join){
-                            $join->on('products.id', '=', 'images.product_id');
-                            $join->where('images.default','1');
-                        })
-                ->whereIn('products.id',session("idcompare"))
-                ->get();
+        if(!empty(session('idcompare'))){
+            return DB::table('products')
+                    ->select('products.*', 'images.address')
+                    ->leftJoin("images",function($join){
+                                $join->on('products.id', '=', 'images.product_id');
+                                $join->where('images.default','1');
+                            })
+                    ->whereIn('products.id',session("idcompare"))
+                    ->get();
+        }else{
+            return null;
+        }
     }
     public function getSpecificationsname(){
-        $return=DB::table('specifications')
-                ->select('specifications.*','specificationname.*','specificationgroup.*')
-                ->leftJoin("specificationname",function($join){
-                     $join->on('specifications.specification_id', '=', 'specificationname.id');
-                })
-                ->leftJoin("specificationgroup",function($join){
-                    $join->on('specificationname.group_id', '=', 'specificationgroup.id');
-                })
-                ->whereIn('specifications.product_id',session("idcompare"))
-                ->get();
-        $arr=[];
-        foreach($return as $key => $item)
-        {
-            $arr[$item->name_group][$item->specification_name][$key] = $item;
+        if(!empty(session('idcompare'))){
+            $return=DB::table('specifications')
+                    ->select('specifications.*','specificationname.*','specificationgroup.*')
+                    ->leftJoin("specificationname",function($join){
+                         $join->on('specifications.specification_id', '=', 'specificationname.id');
+                    })
+                    ->leftJoin("specificationgroup",function($join){
+                        $join->on('specificationname.group_id', '=', 'specificationgroup.id');
+                    })
+                    ->whereIn('specifications.product_id',session("idcompare"))
+                    ->get();
+
+            $arr=[];
+            foreach($return as $key => $item)
+            {
+                $arr[$item->name_group][$item->specification_name][$key] = $item;
+            }
+            return $arr;
+        }else{
+            return null;
         }
-        return $arr;
     }
 }
